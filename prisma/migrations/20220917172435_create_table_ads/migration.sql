4651b5e3bf2d9d279005df781540dfc4196e2cce
Omar <omarucolombari@gmail.com>
2022-09-17 14:24:53 -0300

    Feat: criado model Ads e sua migration

---
+++ prisma/migrations/20220917172435_create_table_ads/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Ads" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "yearsPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "weekDays" TEXT NOT NULL,
+    "hourStart" INTEGER NOT NULL,
+    "hourEnd" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Ads_pkey" PRIMARY KEY ("id")
+);
