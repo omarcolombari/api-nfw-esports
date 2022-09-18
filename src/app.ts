ef8fb56f66744d31e94627b3a87f90232797f75c
Omar <omarucolombari@gmail.com>
2022-09-18 14:48:24 -0300

    Feat: criado middleware de captura de erro

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import routes from "./routes";
+import errorHandling from "./middleware/errorHandling";
+import "express-async-errors";
 
 const app = express();
 export const prisma = new PrismaClient();
@@ -9,5 +11,6 @@ export const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use(errorHandling);
 
 export default app;
---
+++ src/errors/AppError.ts
@@ -0,0 +1,8 @@
+export default class AppError extends Error {
+  statusCode;
+  constructor(message: string, statusCode: number = 400) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
