6e0b5ecf01a0a5bc8f46da5c2fa7c96abd7a41c9
Omar <omarucolombari@gmail.com>
2022-09-18 13:44:44 -0300

    Feat: intregrado todas as rotas

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 export const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import adRouter from "./ad";
+import gameRouter from "./game";
 
 const routes = Router();
+
+routes.use(gameRouter);
+routes.use(adRouter);
 
 export default routes;
